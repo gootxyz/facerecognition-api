6eef7658050798db3708d3a78c79d7e2b4707ae7
hectoragvz <hectoragvz@gmail.com>
2023-10-06 18:21:31 -0700

    Correct config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const PORT = process.env.PORT || 3000;
 
 // Database Connection
 export const DB_HOST = process.env.DB_HOST || '127.0.0.1';
-export const DB_PORT = process.env.DB_HOST || 5432;
+export const DB_PORT = process.env.DB_PORT || 5432;
 export const DB_USER = process.env.DB_USER || 'postgres';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'Gua6195#';
 export const DB_NAME = process.env.DB_NAME || 'facerecognition';
